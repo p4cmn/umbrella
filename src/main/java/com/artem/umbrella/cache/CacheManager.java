77acfab9c6a6fe28809dcca6c05979479eaaf85a
Artsyom Derkach <i.am.artem.derkach@gmail.com>
2024-03-18 22:03:29 +0300

    refactor CacheManager

--- src/main/java/com/artem/umbrella/cache/CacheManager.java
+++ src/main/java/com/artem/umbrella/cache/CacheManager.java
@@ -1,30 +1,40 @@
 package com.artem.umbrella.cache;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Component
 public class CacheManager {
 
-    private final ConcurrentHashMap<String, Object> cache = new ConcurrentHashMap<>();
+    @Value("${cache.limit}")
+    private int limit;
+    private final Map<String, Object> cache =
+            new LinkedHashMap<String, Object>(limit + 1, 0.75f, true) {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry<String, Object> eldest) {
+                    return size() > limit;
+                }
+            };
 
-    public void put(Class<?> clazz, Long id, Object value) {
+    public synchronized void put(Class<?> clazz, Long id, Object value) {
         String key = generateKey(clazz, id);
         cache.put(key, value);
     }
 
-    public Object get(Class<?> clazz, Long id) {
+    public synchronized Object get(Class<?> clazz, Long id) {
         String key = generateKey(clazz, id);
         return cache.get(key);
     }
 
-    public void remove(Class<?> clazz, Long id) {
+    public synchronized void remove(Class<?> clazz, Long id) {
         String key = generateKey(clazz, id);
         cache.remove(key);
     }
 
-    public void update(Class<?> clazz, Long id, Object value) {
+    public synchronized void update(Class<?> clazz, Long id, Object value) {
         remove(clazz, id);
         put(clazz, id, value);
     }
